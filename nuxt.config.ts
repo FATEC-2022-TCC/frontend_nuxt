49455e954ca33ed453bbe3fef88383d0b24cfc8a
Donizete Vida <donizetevida1998@outlook.com>
2022-10-02 13:15:33 -0300

    Unofficial PWA support added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    modules: [
+        '@kevinmarrec/nuxt-pwa'
+    ],
+    pwa: {
+        workbox: {
+            enabled: true
+        }
+    }
 })
