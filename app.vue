e2b9815c3fb8c3330433fc0e916813cf825b929b
Donizete Vida <donizetevida1998@outlook.com>
2022-10-02 13:27:15 -0300

    Automatic page creation by 'pages' folder

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    Hello World
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+const router = useRouter()
+const route = useRoute()
+console.log(route.name)
+</script>
+
+<template>
+    <h1>
+        Hello World
+    </h1>
+</template>
