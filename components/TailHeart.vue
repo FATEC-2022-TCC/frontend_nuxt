58f1eacfc5d1b94c879f082c4403ddfa45626cf9
ale.thomazini <xande.thomazini@gmail.com>
2022-10-25 22:40:14 -0300

    More Animal stuff

--- components/TailHeart.vue
+++ components/TailHeart.vue
@@ -5,7 +5,7 @@ const { modelValue = false } = defineProps({
 </script>
 
 <template>
-    <div class="w-10 h-10 rounded flex cursor-pointer transition-shadow hover:shadow-2xl"
+    <div class="rounded flex cursor-pointer transition-shadow hover:shadow-2xl"
         :class="modelValue ? 'bg-[#BF80FF33]' : 'bg-[#E5E5E5]'" @click="$emit('update:modelValue', !modelValue)">
         <icon name="ant-design:heart-filled" size="2rem" class="m-auto"
             :class="modelValue ? 'text-[#F35353]' : 'text-[#8E8E8E]'" />
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -15,10 +15,8 @@ getContentProjectionInEvidence().then(handle({
         <carousel :autoplay="5000" pause-autoplay-on-hover :items-to-show="1.5" wrap-around
             v-if="contentProjections.length">
             <slide v-for="(cp, index) in contentProjections" :key="cp.id">
-                <div
-                    class="flex flex-col h-52 md:h-80 mx-2 w-full cursor-pointer" 
-                    @click="navigateTo(`/home/content?id=${cp.id}`)"
-                >
+                <div class="flex flex-col h-52 md:h-80 mx-2 w-full cursor-pointer"
+                    @click="navigateTo(`/home/content?id=${cp.id}`)">
                     <img :src="cp.background"
                         class="object-cover object-center w-full h-full rounded-bl-xl rounded-br-xl" />
                     <div class="absolute left-2 right-2 bottom-0 rounded-bl-xl rounded-br-xl"
@@ -37,5 +35,57 @@ getContentProjectionInEvidence().then(handle({
                 <navigation />
             </template>
         </carousel>
+
+        <!-- Blocos de Animais - In Progress -->
+        <!-- <div class="flex justify-evenly my-4">
+
+            <div class="flex flex-col rounded-md w-1/4 p-4 shadow-md shadow-blue-violet">
+                <img :src="contentProjections[0].background" class="rounded-md mb-2" />
+                <div class="mb-2">
+                    <h1 class="text-center text-4xl font-amatic-sc text-blue-violet font-bold">
+                        {{ contentProjections[0].title }}
+                    </h1>
+                    <p class="text-center text-xl font-amatic-sc">
+                        {{ contentProjections[0].description }}
+                    </p>
+                </div>
+                <div class="flex w-full justify-around gap-4">
+                    <tail-heart class="p-2" />
+                    <tail-button-blue-violet title="Adote agore" class="flex-1" />
+                </div>
+            </div>
+
+            <div class="flex flex-col rounded-md w-1/4 p-4 shadow-md shadow-blue-violet">
+                <img :src="contentProjections[0].background" class="rounded-md mb-2" />
+                <div class="mb-2">
+                    <h1 class="text-center text-4xl font-amatic-sc text-blue-violet font-bold">
+                        {{ contentProjections[0].title }}
+                    </h1>
+                    <p class="text-center text-xl font-amatic-sc">
+                        {{ contentProjections[0].description }}
+                    </p>
+                </div>
+                <div class="flex w-full justify-around gap-4">
+                    <tail-heart class="p-2" />
+                    <tail-button-blue-violet title="Adote agore" class="flex-1" />
+                </div>
+            </div>
+
+            <div class="flex flex-col rounded-md w-1/4 p-4 shadow-md shadow-blue-violet">
+                <img :src="contentProjections[0].background" class="rounded-md mb-2" />
+                <div class="mb-2">
+                    <h1 class="text-center text-4xl font-amatic-sc text-blue-violet font-bold">
+                        {{ contentProjections[0].title }}
+                    </h1>
+                    <p class="text-center text-xl font-amatic-sc">
+                        {{ contentProjections[0].description }}
+                    </p>
+                </div>
+                <div class="flex w-full justify-around gap-4">
+                    <tail-heart class="p-2" />
+                    <tail-button-blue-violet title="Adote agore" class="flex-1" />
+                </div>
+            </div>
+        </div> -->
     </div>
 </template>
