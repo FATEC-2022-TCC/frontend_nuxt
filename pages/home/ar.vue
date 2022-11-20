d6eea19b0d586410fb74ef15290b66386a3ae683
Donizete Vida <donizetevida1998@outlook.com>
2022-11-20 15:11:10 -0300

    AR code refactor

--- pages/home/ar.vue
+++ pages/home/ar.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import type { XRSystem } from 'webxr'
-
 import * as THREE from "three"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DragControls } from 'three/examples/jsm/controls/DragControls'
 
 const loader = new GLTFLoader();
 
@@ -15,97 +14,95 @@ async function isSessionSupported(): Promise<boolean> {
 
 async function requestSession() {
     const supported = await isSessionSupported()
-    if (supported) {
-        try {
-            const flower = await loadModel("https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf")
-            const reticle = await loadModel("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf")
+    if (!supported) {
+        //AR session isn't supported. Try it on another device.
+        alert("A sessão de AR não é suportada. Tente em outro dispositivo.")
+        return
+    }
+    try {
+        // const flower = await loadModel("https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf")
+        const reticle = await loadModel("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf")
+        const model = await loadModel("/model.glb")
+        model.visible = false
 
-            const canvas = document.createElement('canvas')
-            const gl = canvas.getContext('webgl', { xrCompatible: true })
-            if (!gl) throw "webgl is null"
+        const canvas = document.createElement('canvas')
+        const gl = canvas.getContext('webgl', { xrCompatible: true })
+        if (!gl) throw "webgl is null"
 
-            const scene = new THREE.Scene();
+        const scene = new THREE.Scene();
 
-            const light = new THREE.DirectionalLight(0xffffff, 0.3);
-            light.position.set(10, 15, 10);
+        const light = new THREE.AmbientLight(0xffffff, 1);
 
-            scene.add(light)
-            //scene.add(flower)
-            scene.add(reticle)
+        scene.add(light)
+        scene.add(model)
+        scene.add(reticle)
 
-            const camera = new THREE.PerspectiveCamera();
-            camera.matrixAutoUpdate = false;
+        const camera = new THREE.PerspectiveCamera();
+        camera.matrixAutoUpdate = false;
 
-            const renderer = new THREE.WebGLRenderer({
-                antialias: true,
-                alpha: true,
-                context: gl,
-                canvas: gl.canvas
-            })
-            renderer.autoClear = true
+        const renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true,
+            context: gl,
+            canvas: gl.canvas
+        })
+        renderer.autoClear = true
 
-            const xr = navigator.xr
-            if (!xr) throw "XRSystem is null"
-            const session = await xr.requestSession('immersive-ar', {
-                requiredFeatures: ['local', 'hit-test'],
-            })
-            session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) })
+        const xr = navigator.xr
+        if (!xr) throw "XRSystem is null"
+        const session = await xr.requestSession('immersive-ar', {
+            requiredFeatures: ['local', 'hit-test'],
+        })
+        session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) })
 
-            session.addEventListener('select', _ => {
-                const clone = flower.clone()
-                clone.position.copy(reticle.position)
-                scene.add(clone)
-            })
+        session.addEventListener('select', _ => model.position.copy(reticle.position))
 
-            //where session was created
-            const local = await session.requestReferenceSpace('local')
+        //where session was created
+        const local = await session.requestReferenceSpace('local')
 
-            //current session position
-            const viewer = await session.requestReferenceSpace('viewer');
+        //current session position
+        const viewer = await session.requestReferenceSpace('viewer');
 
-            if (!session.requestHitTestSource) throw "requestHitTestSource is null"
+        const hitTestSource = session.requestHitTestSource && await session.requestHitTestSource({ space: viewer });
 
-            const hitTestSource = await session.requestHitTestSource({ space: viewer });
+        if (!hitTestSource) throw "hitTestSource is null"
 
-            if (!hitTestSource) throw "hitTestSource is null"
+        const onXRFrame = (time: DOMHighResTimeStamp, frame: XRFrame) => {
+            session.requestAnimationFrame(onXRFrame);
+            gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer?.framebuffer ?? null)
 
-            const onXRFrame = (time: DOMHighResTimeStamp, frame: XRFrame) => {
-                session.requestAnimationFrame(onXRFrame);
-                gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer?.framebuffer ?? null)
+            const pose = frame.getViewerPose(local);
 
-                const pose = frame.getViewerPose(local);
+            if (pose) {
+                const view = pose.views[0];
 
-                if (pose) {
-                    const view = pose.views[0];
+                const viewport = session.renderState.baseLayer?.getViewport(view);
+                if (!viewport) return
+                renderer.setSize(viewport.width, viewport.height)
 
-                    const viewport = session.renderState.baseLayer?.getViewport(view);
-                    if (!viewport) return
-                    renderer.setSize(viewport.width, viewport.height)
+                camera.matrix.fromArray(view.transform.matrix)
+                camera.projectionMatrix.fromArray(view.projectionMatrix);
+                camera.updateMatrixWorld(true);
+            }
 
-                    camera.matrix.fromArray(view.transform.matrix)
-                    camera.projectionMatrix.fromArray(view.projectionMatrix);
-                    camera.updateMatrixWorld(true);
-
-                }
-
-                const hitTestResults = frame.getHitTestResults(hitTestSource);
-
-                if (hitTestResults.length > 0) {
-                    const hitPose = hitTestResults[0].getPose(local);
-                    if (hitPose) {
-                        reticle.position.set(hitPose.transform.position.x, hitPose.transform.position.y, hitPose.transform.position.z)
-                        reticle.updateMatrixWorld(true);
-                    }
-                }
-
+            for (const result of frame.getHitTestResults(hitTestSource)) {
+                const pose = result.getPose(local)
+                if (!pose) continue
+                reticle.position.set(
+                    pose.transform.position.x,
+                    pose.transform.position.y,
+                    pose.transform.position.z
+                )
+                reticle.updateMatrixWorld(true)
                 renderer.render(scene, camera)
             }
-            session.requestAnimationFrame(onXRFrame);
-        } catch (e) {
-            alert("Something went wrong on VR session: " + JSON.stringify(e))
+            renderer.render(scene, camera)
         }
-    } else {
-        alert("VR session is not supported")
+        session.requestAnimationFrame(onXRFrame);
+    } catch (e) {
+        console.log(e)
+        //Something went wrong when loading the AR session. Refresh the page and try again!
+        alert("Alguma coisa deu errada ao carregar a sessão de AR. Atualize página e tente novamente!")
     }
 }
 </script>
