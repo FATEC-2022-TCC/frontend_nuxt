6f4d31cf218e78927bb057f141b21470bbe1126a
Donizete Vida <donizetevida1998@outlook.com>
2022-12-12 03:36:43 -0300

    More fixes

--- pages/admin/index/users/view.vue
+++ pages/admin/index/users/view.vue
@@ -22,23 +22,41 @@ function start() {
 
 <template>
     <tail-loading-page class="flex flex-col p-4" :has-remote-error="hasRemoteError" :is-loading="!response">
-        <h1 class="font-amatic-sc text-6xl">
-            Visualizar usuário
-        </h1>
-        <br>
-        <h1 class="text-4xl font-amatic-sc">Nome: &nbsp;</h1>
-        <p> {{ response.name }}</p>
-        <br>
-        <h1 class="text-4xl font-amatic-sc">Nome de usuário: &nbsp;</h1>
-        <p> {{ response.username }}</p>
-        <br>
-        <h1 class="text-4xl font-amatic-sc">Email: &nbsp;</h1>
-        <p> {{ response.email }}</p>
-        <br>
-        <h1 class="text-4xl font-amatic-sc">Tipo de usuário: &nbsp;</h1>
-        <p> {{ response.authority }}</p>
-        <br>
-        <h1 class="text-4xl font-amatic-sc">Situação do usuário: &nbsp;</h1>
-        <p> O usuário está {{ response.isActive ? 'ativo' : 'inativo' }}</p>
+        <template v-if="response">
+            <h1 class="font-amatic-sc text-6xl">
+                Visualizar usuário
+            </h1>
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Nome: &nbsp;</h1>
+            <p> {{ response.name }}</p>
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Nome de usuário: &nbsp;</h1>
+            <p> {{ response.username }}</p>
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Tipo de usuário: &nbsp;</h1>
+            <p> {{ response.authority }}</p>
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Situação do usuário: &nbsp;</h1>
+            <p> O usuário está {{ response.isActive ? 'ativo' : 'inativo' }}</p>
+            <br><br>
+            <h1 class="font-amatic-sc text-6xl">
+                Informações privadas
+            </h1>
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Foto de identificação: &nbsp;</h1>
+            <img :src="response.privateInfo.picture">
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Email: &nbsp;</h1>
+            <p> {{ response.privateInfo.email }}</p>
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Telefone: &nbsp;</h1>
+            <p> {{ response.privateInfo.telephony }}</p>
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Documento: &nbsp;</h1>
+            <p> {{ response.privateInfo.identification }}</p>
+            <br>
+            <h1 class="text-4xl font-amatic-sc">Endereço: &nbsp;</h1>
+            <p> {{ response.privateInfo.address }}</p>
+        </template>
     </tail-loading-page>
 </template>
